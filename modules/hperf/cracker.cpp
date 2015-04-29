6db6178ec3a6b73745aa37b8d218d3b6e757044e
Gaston Traberg <samelat@gmail.com>
2015-04-29 02:45:10 -0300

    It use login instead callback, thats all

--- modules/hperf/cracker.cpp
+++ modules/hperf/cracker.cpp
@@ -4,13 +4,27 @@ void Cracker::crack(bp::list usernames, bp::list passwords, bp::list pairs) {
     
     using string_iterator = bp::stl_input_iterator<const char *>;
 
+    status_t result;
+
     for(string_iterator usr(usernames); usr != string_iterator(); usr++) {
         for(string_iterator pwd(passwords); pwd != string_iterator(); pwd++) {
-            callback(*usr, *pwd);
+            result = login(*usr, *pwd);
+            switch(result) {
+                case status_t::SUCCESS: callback(*usr, *pwd); break;
+                case status_t::FAILED:  break;
+                case status_t::ERROR:   break; // This will be handle.
+            }
         }
     }
 
     for(bp::stl_input_iterator<bp::tuple> p(pairs); p != bp::stl_input_iterator<bp::tuple>(); p++) {
-        callback(bp::extract<const char *>((*p)[0])(), bp::extract<const char *>((*p)[1])());
+        result = login(bp::extract<const char *>((*p)[0])(), bp::extract<const char *>((*p)[1])());
+        switch(result) {
+            case status_t::SUCCESS: callback(bp::extract<const char *>((*p)[0])(),
+                                             bp::extract<const char *>((*p)[1])());
+                break;
+            case status_t::FAILED:  break;
+            case status_t::ERROR:   break; // This will be handle.
+        }
     }
 }
