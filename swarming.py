454a137532ae8e835cf2dad92c8c01a941bf2f2a
Gaston Traberg <gtraberg@cert.unlp.edu.ar>
2015-08-04 13:47:01 -0300

    logger system is working

--- swarming.py
+++ swarming.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 
-import logging
 import argparse
+
+import json
+import logging
+import logging.config
 
 from modules.logging import setupLogging
 from units.core.core import Core
 
+with open('config/log.json', 'r') as log_conf:
+    json_conf = json.loads(log_conf.read())
+    logging.config.dictConfig(json_conf)
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-s', '--session', help='session name', nargs=1, default='default')
-parser.add_argument('-l', '--log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], help='set log level')
+parser.add_argument('-l', '--log-level', choices=['debug', 'info', 'warning', 'error', 'critical'])
 
 args = parser.parse_args()
 
-setupLogging("")
+# Set root-logger handlers log level
+if args.log_level:
+    rlogger = logging.getLogger()
+    log_level = getattr(logging, args.log_level.upper())
 
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
+    for handler in rlogger.handlers:
+        handler.setLevel(log_level)
 
-root_logger.debug("ALGO")
+#logger = logging.getLogger('unit')
+#logger.info("Error!!!!")
 
-logger = logging.getLogger('user')
-
-logger.info("Error!!!!")
-
-#core = Core()
-#core.start()
+core = Core()
+core.start()
